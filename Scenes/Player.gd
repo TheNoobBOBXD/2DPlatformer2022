831a62d9074ab32d488b590343d72eb056e0266a
TheNoobBOBXD <noobbobxd@gmail.com>
2022-03-07 19:28:30 +1300

    add zoom and texture for floor
    cool beans done at home

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 export (int) var speed = 120 
 export (int) var jump_speed = -180
-export (int) var gravity = 200
+export (int) var gravity = 400
 export (int) var slide_speed = 400
 
 var velocity = Vector2.ZERO
